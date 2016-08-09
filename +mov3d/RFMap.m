d5d88f11f7b3b9d51ed211dcf6b40b7113bf52f3
Emmanouil Froudarakis <olakiril@hotmail.com>
2016-08-09 10:33:31 -0500

    rfmap

--- +mov3d/RFMap.m
+++ +mov3d/RFMap.m
@@ -3,7 +3,7 @@ mov3d.RFMap (computed) # population RF
 -> rf.Sync
 ---
 map                    : longblob        # standard deviation from center of population RF
-rf_idx                 : longblob        # index of subbins with stimulus in pop RF {trials}(subbins)               
+rf_idx                 : longblob        # index of subbins with stimulus in pop RF {trials}(subbins)
 rf_trials              : longblob        # trial index (trial indexes)
 %}
 
@@ -20,25 +20,32 @@ classdef RFMap < dj.Relvar & dj.AutoPopulate
         
         function makeTuples(self, key)
             
-            % binsize = 
-            
+            % binsize =
+          
+            % get RFs
             [xloc, yloc, r, c] = fetchn(monet.Fit & key,'x','y','radius','contrast');
-            [width, height] = fetchn(monet.RF & key, 'degrees_x', 'degrees_y');
+            
+            % convert to pixel space
+            sess = fetch(rf.Sync*psy.Session & key,'resolution_x','resolution_y','monitor_distance','monitor_size');
+            rect = [sess.resolution_x sess.resolution_y];            
+            degPerPix = 180/pi*sess.monitor_size*2.54/norm(rect(1:2))/sess.monitor_distance;
+            xloc = xloc/degPerPix;
+            yloc = yloc/degPerPix;
+            
             m = fitgmdist([xloc,yloc],1);
             mu = m.mu;
             C = m.Sigma;
-            [x,y] = meshgrid(-125:124,-125:124);
-
+            [x,y] = meshgrid(-225:224,-225:224);
             X=[x(:) y(:)];
-
             X = bsxfun(@minus, X, mu);
             d = sum((X /C) .* X, 2);
-
-           % stimulus_trial_xy_position
-           % stimulus_bin_xy_position inside circle
- 
- 
-
+            
+                      
+            % stimulus_trial_xy_position
+            % stimulus_bin_xy_position inside circle
+            
+            
+            
             self.insert(tuple)
             
         end
